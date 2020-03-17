9b52eed89772de5b257d7f696763c1ee3d74b11f
djoun52 <simon.rioux.trv@gmail.com>
2020-03-17 15:39:30 +0100

    dernier vérif

--- algo_PHP1/exo15.php
+++ algo_PHP1/exo15.php
@@ -1,19 +1,19 @@
 <a href=" ./index.php" title="index"> index</a>
 <br>
 <?php
-class person
+class Person
 {
     // Properties
-    public $nom ;
-    public $prenom;
-    public $age ;
+    private $nom ;
+    private $prenom;
+    private $date_de_naissence;
    
    
-    function __construct($c_nom,$c_prenom,$c_age)
+    function __construct($c_nom,$c_prenom,$c_date_de_naissence)
     {
         $this-> nom = $c_nom;
         $this-> prenom = $c_prenom;
-        $this-> age = $c_age;
+        $this-> date_de_naissence = $c_date_de_naissence;
 
     }
      // Methods
@@ -33,18 +33,18 @@ class person
     {
         $this->prenom = $p_prenom;
     }
-     function get_age()
+     function get_date_de_naissence()
     {
-        return $this->age;
+        return $this->date_de_naissence;
     }
-    function set_age($p_age)
+    function set_date_de_naissence($p_date_de_naissence)
     {
-        $this->age= $p_age;
+        $this->date_de_naissence= $p_date_de_naissence;
     }
     public function id()
     {
         $date= new DateTime();
-        $date1= new DateTime ($this->get_age());
+        $date1= new DateTime ($this->get_date_de_naissence());
         $diff=date_diff($date1,$date);
         echo  $this->get_nom(). "  "  .$this->get_prenom() . " " . $diff->format("à %y ans") . "<br>"; 
     }
